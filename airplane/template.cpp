1fa995928b0e58cb797915f9e980c6c573f15eb2
Gyumin <guminh@naver.com>
2026-05-03 09:44:07 +0900

    ADD Settings

---
+++ airplane/template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // solve here
+    cout << "Hello\n";
+
+    return 0;
+}
